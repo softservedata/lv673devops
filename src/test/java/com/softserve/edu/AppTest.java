70648cbb7fef320512a474b9ae7bf9d434c42461
bebebe <hahaha@gmail.com>
2022-02-08 14:58:00 +0200

    Read Environment

--- src/test/java/com/softserve/edu/AppTest.java
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -8,6 +8,7 @@ public class AppTest {
 
     @Test
     public void testApp() {
+ System.out.println("***System.getenv().MY_PASSWORD = " + System.getenv().get("MY_PASSWORD"));
         System.out.println("\t***** AppTest done");
         Assert.assertTrue(true);
     }
