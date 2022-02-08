3932717e0871c2d686822c112b726507acbca4d6
bebebe <hahaha@gmail.com>
2022-02-08 14:27:05 +0200

    Template Java Project

---
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -0,0 +1,15 @@
+package com.softserve.edu;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        System.out.println("\t***** AppTest done");
+        Assert.assertTrue(true);
+    }
+
+}
